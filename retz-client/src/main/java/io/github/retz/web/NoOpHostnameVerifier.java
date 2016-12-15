bb6c3ddffba3b0e663ebc46bf516d613f4858d44
Takuya UESHIN <ueshin@nautilus-technologies.com>
2016-12-15 23:47:10 +0900

    Use SSLSocketFactory and HostnameVerifier in each connection.

--- retz-client/src/main/java/io/github/retz/web/NoOpHostnameVerifier.java
+++ retz-client/src/main/java/io/github/retz/web/NoOpHostnameVerifier.java
@@ -16,25 +16,13 @@
  */
 package io.github.retz.web;
 
-import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 
-import javax.net.ssl.X509TrustManager;
-
-// DANGER ZONE: this disables TLS certification and allow self-signed certificate
-// do not use this over internet
-// Copyright notice: this function is based on StackOverflow:19540289
-public class WrongTrustManager implements X509TrustManager {
+public class NoOpHostnameVerifier implements HostnameVerifier {
 
     @Override
-    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-        return null;
-    }
-
-    @Override
-    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-    }
-
-    @Override
-    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+    public boolean verify(String hostname, SSLSession session) {
+        return true;
     }
 }
